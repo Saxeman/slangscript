23cb87ad7f8489c7d8f2e46a5c0ef5a53209cb8a
Saxeman <ajsaxe@umich.edu>
2022-11-17 22:01:10 -0600

    figured out a little bit of a approach towards building the lexer

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -1,21 +1,26 @@
 #include <fstream>
 #include <iostream>
+#include <string>
 #include "../includes/lexer.h"
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    cout << "hello world" << endl;
     string filename = argv[1];
     cout << filename << endl;
-    //ifstream infile(filename);
-    // this is just a test edit to make sure that git is working
+    ifstream infile(filename);
 
-    //string line;
-    //while (getline(infile, line)) {
-        //cout << "this is a line in our code" << endl;
-        //cout << line << endl;
-    //}
+    string line;
+    // we need to read individual characters and essentially check whether the character that we 
+    // are reading would be a part of our built in functionality, then add that to our lexicon
+
+    // Another alternative would be to read each individual word (BUT HOW DO WE HANDLE WITH WHITE SPACES???) 
+    // and just add it to our lexicon
+
+    // I think that the best course of action here is to simply watch the youtube tutorials and continue from there.
+    while (getline(infile, line, ' ')) {
+        cout << line << endl;
+    }
     cout << "if this prints that means our while loop didnt run :(" << endl;
     return 0;
 }
