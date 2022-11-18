99d65879c3c91dc744ec1aa081d373c49a6e930a
ajsaxe <ajsaxe@umich.edu>
2022-11-17 18:02:33 -0600

    added better file organization as well as a semi-working makefile - FUCK MAKE CLEAN

---
+++ includes/lexer.h
@@ -0,0 +1,5 @@
+using namespace std;
+
+int generic_func();
+
+void new_function();
--- src/lexer.cpp
+++ src/lexer.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <iostream>
+#include "../includes/lexer.h"
 
 using namespace std;
 
@@ -7,14 +8,18 @@ int main(int argc, char *argv[]) {
     cout << "hello world" << endl;
     string filename = argv[1];
     cout << filename << endl;
-    ifstream infile(filename);
+    //ifstream infile(filename);
     // this is just a test edit to make sure that git is working
 
-    string line;
-    while (getline(infile, line)) {
-        cout << "this is a line in our code" << endl;
-        cout << line << endl;
-    }
+    //string line;
+    //while (getline(infile, line)) {
+        //cout << "this is a line in our code" << endl;
+        //cout << line << endl;
+    //}
     cout << "if this prints that means our while loop didnt run :(" << endl;
     return 0;
+}
+
+int generic_func() {
+    return 1;
 }
